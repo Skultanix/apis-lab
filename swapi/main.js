edda1c44c342c7d2929c66dbb298e59905084092
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-03-02 16:44:20 -0700

    Axios requests and functionality complete.

--- swapi/main.js
+++ swapi/main.js
@@ -1,4 +1,7 @@
+
+
 const getResBtn = document.querySelector('button');
+const getBody = document.querySelector('body')
 const baseURL = `https://swapi.dev/api/planets/`
 const searchURL = `?search=Alderaan`
 
@@ -7,7 +10,21 @@ function buttonClick() {
      //console.log(`Button clicked.`)
      axios.get(baseURL + searchURL) 
         .then((res) => {
-            console.log(res.data)
+            const alderaanRes = res.data.results[0].residents;
+            //console.log(alderaanRes)
+            for(i = 0; i < alderaanRes.length; i++) {
+                let resident = alderaanRes[i]
+                axios.get(resident)
+                    .then(res =>  {
+                        const resName = res.data.name;
+                        let h2 = document.createElement(`h2`)
+                        let node = document.createTextNode(resName)
+                        h2.appendChild(node)
+                        getBody.appendChild(h2)
+
+                    })
+            }
+
         })
         .catch(error => {
             console.log(error)
